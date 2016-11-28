6f6fb6b6564a8d958f64d87a5487704ba52f7831
Grégoire Terras <gterras@gmail.com>
2016-11-28 23:59:32 +0100

    working oauth2 login system

--- web/index.php
+++ web/index.php
@@ -11,23 +11,60 @@
   <link rel="stylesheet" href="assets/css/styles.css" media="all">
 </head>
 <body>
+<?php
 
-  <?php
-  require __DIR__ . '/../vendor/autoload.php';
-  use twig\twig;
-  $loader = new Twig_Loader_Filesystem('../blocks');
-  $twig = new Twig_Environment($loader, array(
-    'cache' => '/dev/shm',
-    'debug' => true));
-    ?>
 
-    <?php
-    echo $twig->render('forms/form.html', array('the' => 'vargiables', 'go' => 'here'));
-    ?>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="assets/js/jquery.modal.min.js"></script>
-    <script src="assets/js/main.js"></script>
+ // header('Location: '.$_SERVER['REQUEST_URI']);
+
+
+
+include "ClientCredentials.php";
+
+session_start();
+
+if (empty($_SESSION['access_token'])) {
+
+// Generate random value for use as the 'state'.  Mitigates
+// cross site request forgery attacks.
+$_SESSION['state'] = rand(0,999999999);
+
+
+// This is the Microsoft authentication service URL used to initiate the OAuth authentication flow
+$authorizationUrlBase = 'http://preview.gitlab.local/oauth/authorize';
+
+// URL parameters used to request an authorization token
+$queryParams = array(
+    'client_id' => $clientId,
+    'redirect_uri' => 'http://localhost/~kursus/git/nectar/web' . $redirectUriPath,
+    'response_type' => 'code',
+    'state' => $_SESSION['state']
+);
+
+// Microsoft Authentication service url and params
+$goToUrl = $authorizationUrlBase . '?' . http_build_query($queryParams);
+?>
+
+<h2>Bing Ads OAuth Demo</h2>
+
+<p>This application would like to manage your Bing Ads data. Click below to login and authorize this application.</p>
+
+<p>When you click OK below, you'll be redirected to the following URI:</p>
+<p><?php echo $goToUrl ?></p>
+
+<input type="button" onClick="return window.location='<?php echo $goToUrl;?>';" value="OK" />
+
+<?php
+
+}
+else {
+  $accessToken = $_SESSION['access_token'];
+
+  printf("Implement code to retrieve data with access token: %s", $accessToken);
+}
+
+ ?>
+
   </body>
   </html>
 
@@ -35,36 +72,4 @@
 
 
 
-
-  <!-- <div id="login-form" class="w50" style="display:none;"> -->
-
-<!--
-<section class="flex-container">
-  <span class="flex-item-center">
-    <p>I am centered</p>
-  </span>
-</section>
--->
-
-
-<!-- Link to open the modal -->
-
-<!-- <section class="grid test">
-
-  <div class="push center three-quarters">.push</div>
-
-</section> -->
-
-<?php
-	//require __DIR__ . '/vendor/autoload.php';
-
-	// use AdamBrett\ShellWrapper\Runners\Exec;
-	// use AdamBrett\ShellWrapper\Command\Builder as CommandBuilder;
-
-	// $shell = new Exec();
-	// $command = new CommandBuilder('/usr/bin/git');
-	// $command->addSubCommand('init')
-	// 		->addArgument('bare','/home/kursus/websites/git/gitcreator/coco');
-	// $shell->run($command);
-?>
 
